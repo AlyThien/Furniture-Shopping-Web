cf4b84d46ba3510fbd401b4d216304b769e32266
Trần Thanh Hà <23520415@gm.uit.edu.vn>
2025-11-25 21:54:47 +0700

    Update LOGIN_EMAIL_KEY to use 'userEmail'

--- js/personal-info.js
+++ js/personal-info.js
@@ -1,6 +1,6 @@
 const USER_DATA_KEY = 'Haguchi_UserData';
 const ORDER_DATA_KEY = 'Haguchi_MockOrders';
-const LOGIN_EMAIL_KEY = 'Haguchi_LoggedInUser'; // KEY ĐỒNG BỘ với login.js
+const LOGIN_EMAIL_KEY = 'userEmail'; // KEY ĐỒNG BỘ với login.js
 
 //Nhóm 9: Dữ liệu Đơn hàng GIẢ LẬP
 const defaultMockOrders = [
